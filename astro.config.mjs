6ce1e84b2edb5061097385b3a12dc642f38ef335
Tadas Petra <tadas@tadaspetra.com>
2024-10-04 15:59:08 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  site: 'https://agoraio-community.github.io/openai-demo/',
+  base: 'openai-demo',
 });
