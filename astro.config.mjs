93c1bc1afd665c2e379733546bf4a750d55f0e60
Tadas Petra <tadas@tadaspetra.com>
2024-10-07 14:34:36 -0500

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [react(), tailwind()],
   site: 'https://agoraio-community.github.io',
-  base: 'openai-demo'
+  base: 'openai-demo',
+  adapter: vercel()
 });
